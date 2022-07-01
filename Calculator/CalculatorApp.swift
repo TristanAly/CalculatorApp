0041e657597386c2e8581bd67d131dc676107bdd
AlyTristan <tristan.aly@gmail.com>
2022-07-01 11:32:27 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tristan Aly on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
